02d5a1a0f189613658c241066642ea981ddb9ba2
S M Shakib Hossain <smsakib2403@gmail.com>
2021-03-19 00:36:48 +0600

    Create Day 1: Data Types.cpp

---
+++ Day 1: Data Types.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int j;
+double e;
+string t;
+cin>>j>>e;
+cin.ignore();
+getline(cin, t);
+cout<<i+j<<'\n';
+cout<<fixed<<setprecision(1)<<d+e <<'\n';
+cout<<s<<t;
+}
